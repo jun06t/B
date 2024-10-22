0b807d0a2b5beb97aac302d2617cecd9b5f87ed9
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2024-10-22 19:25:48 +0900

    v1.2.0

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/jun06t/B
 
 go 1.23.2
+
+require github.com/jun06t/C v1.4.0
+
+require github.com/jun06t/D v1.2.0 // indirect
--- lib.go
+++ lib.go
@@ -1,7 +1,14 @@
 package B
 
+import (
+	"fmt"
+
+	"github.com/jun06t/C"
+)
+
 const version = "1.1.0"
 
 func Func() string {
+	fmt.Println(C.Func())
 	return version
 }
